7b4d8a66e435238e40622b7c6f32dc64700987d6
Assos Assos <assos+forge@centrale-marseille.fr>
2017-06-06 19:50:19 +0200

    Creation of site: forumtest

--- drupal7/sites/sites.php
+++ drupal7/sites/sites.php
@@ -91,3 +91,4 @@ $sites['assos.centrale-marseille.fr.accueiltest'] = 'assos.centrale-marseille.fr
 $sites['assos.centrale-marseille.fr.rgrondin'] = 'assos.centrale-marseille.fr.rgrondin';
 $sites['assos.centrale-marseille.fr.clubdrupal'] = 'assos.centrale-marseille.fr.clubdrupal';
 $sites['assos.centrale-marseille.fr.accueilold'] = 'assos.centrale-marseille.fr.accueilold';
+$sites['assos.centrale-marseille.fr.forumtest'] = 'assos.centrale-marseille.fr.forumtest';
