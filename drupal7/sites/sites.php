df3f21f68e501bdf29db0a6a5ce41e91d1438ab2
Assos Assos <assos+forge@centrale-marseille.fr>
2019-02-05 14:29:03 +0100

    Deletion of site: lwillem

--- drupal7/sites/sites.php
+++ drupal7/sites/sites.php
@@ -92,7 +92,6 @@ $sites['assos.centrale-marseille.fr.accueilold'] = 'assos.centrale-marseille.fr.
 $sites['assos.centrale-marseille.fr.forumtest'] = 'assos.centrale-marseille.fr.forumtest';
 $sites['assos.centrale-marseille.fr.epold'] = 'assos.centrale-marseille.fr.epold';
 $sites['assos.centrale-marseille.fr.uaecm'] = 'assos.centrale-marseille.fr.uaecm';
-$sites['assos.centrale-marseille.fr.lwillem'] = 'assos.centrale-marseille.fr.lwillem';
 $sites['assos.centrale-marseille.fr.mlannes'] = 'assos.centrale-marseille.fr.mlannes';
 $sites['assos.centrale-marseille.fr.acohen'] = 'assos.centrale-marseille.fr.acohen';
 $sites['assos.centrale-marseille.fr.ancienbde'] = 'assos.centrale-marseille.fr.ancienbde';
